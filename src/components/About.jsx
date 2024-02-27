2e248de129ca4c49e0ebfd1a0942f62b4e658938
adith-p <adithprakash008@gmail.com>
2024-02-27 18:22:33 +0530

    removed emojies

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,10 +9,10 @@ const About = () => {
                 <div className="about-text  p-2.5 text-white">
                     <p className="title">As a developer...</p>
                     <p>
-                        my passion lies in creating elegant and effective solutions to complex challenges. Having a strong foundation in software development, especially backend technologies like <strong>Python</strong>, <strong>Django</strong>, <strong>Django Rest Framework</strong>, <strong>FastAPI</strong>, <strong>PostgreSQL</strong>, <strong>Celery</strong>, and <strong>Redis</strong>, I enjoy backend development.<span className="emoji">🚀</span>
+                        my passion lies in creating elegant and effective solutions to complex challenges. Having a strong foundation in software development, especially backend technologies like <strong>Python</strong>, <strong>Django</strong>, <strong>Django Rest Framework</strong>, <strong>FastAPI</strong>, <strong>PostgreSQL</strong>, <strong>Celery</strong>, and <strong>Redis</strong>, I enjoy backend development.
                     </p>
                     <p>
-                        Although I dabbled in front-end technologies like <strong>HTML5</strong>, <strong>CSS3</strong>, and <strong>JavaScript</strong>, I found my true calling in back-end development and chose to focus my efforts there.<span className="emoji">💡</span>
+                        Although I dabbled in front-end technologies like <strong>HTML5</strong>, <strong>CSS3</strong>, and <strong>JavaScript</strong>, I found my true calling in back-end development and chose to focus my efforts there.
                     </p>
                     <p >
                         With the goal of improving efficiency, enhancing user experience, and maintaining the highest standards of code quality, I am constantly looking for opportunities to hone my skills In my journey throughout, I have handled a variety of projects, ranging from straightforward backend applications to much more complex ones. My expertise extends to a variety of development tools and frameworks including <strong>Git</strong>, <strong>VS Code</strong>, <strong>Linux</strong>, and <strong>Nginx</strong>. I maintain a strong interest in exploring new backend technologies and am dedicated to continuous learning and development.
